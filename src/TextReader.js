0dea9ff766c9cbc5cf510c55a6de0afce5395c10
polan233 <1292437680@qq.com>
2023-02-10 20:05:33 +0800

    获取文章内容的api的前端响应处理写好了

--- src/TextReader.js
+++ src/TextReader.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {GlobalOutlined } from '@ant-design/icons';
 import { Typography,Button } from 'antd';
+import { getContentByTitle } from "./axios/api";
 const { Title, Paragraph, Text, Link } = Typography;
 const testText=`AntV 是蚂蚁金服全新一代数据可视化解决方案，致力于提供一套简单方便、专业可靠、不限可能的数据可视化最佳实践。得益于丰富的业务场景和用户需求挑战，AntV 经历多年积累与不断打磨，已支撑整个阿里集团内外 20000+ 业务系统，通过了日均千万级 UV 产品的严苛考验。
 我们正在基础图表，图分析，图编辑，地理空间可视化，智能可视化等各个可视化的领域耕耘，欢迎同路人一起前行。\r\nAntV 是蚂蚁金服全新一代数据可视化解决方案，致力于提供一套简单方便、专业可靠、不限可能的数据可视化最佳实践。得益于丰富的业务场景和用户需求挑战，AntV 经历多年积累与不断打磨，已支撑整个阿里集团内外 20000+ 业务系统，通过了日均千万级 UV 产品的严苛考验。
@@ -20,6 +21,7 @@ class TextReader extends React.Component{
         this.state={
             paragraphs:[],
         }
+        this.handleContent=this.handleContent.bind(this);
     }
     buildParagraph(textStr_p){
         console.log("text_p",textStr_p)
@@ -37,14 +39,23 @@ class TextReader extends React.Component{
                 console.log("paragraph building",temp)
             }
         }
+        res.push(<Paragraph className="textReader-p">{temp}</Paragraph>)
         return res;
     }
-    componentDidMount(){
-        //TO-DO api请求获取文章内容并处理
-        const text=this.buildParagraph(testText)
+    handleContent(response){
+        console.log("handleContent",response)
+        const text=this.buildParagraph(response.data.data.content)
         this.setState({
             paragraphs:text
         })
+    }
+    componentDidMount(){
+        //TO-DO api请求获取文章内容并处理
+        getContentByTitle(this.props.title,this.handleContent);
+        // const text=this.buildParagraph(testText)
+        // this.setState({
+        //     paragraphs:text
+        // })
     }
     render(){
         return(
