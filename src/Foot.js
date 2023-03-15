83c33c899bfcfd5b3b87fd63e9d977f458257502
polan233 <1292437680@qq.com>
2023-03-15 14:13:12 +0800

    chore&feat: 暂时隐藏无用的搜索框, 加入联系我们弹窗

--- src/Foot.js
+++ src/Foot.js
@@ -1,6 +1,7 @@
 
 import React, { useState } from 'react';
 import { Col, Row,Button } from 'antd';
+import {  notification, Space } from 'antd';
 import beianlogo from "./img/beian.png"
 import './App.css'
 
@@ -46,7 +47,7 @@ class Links extends React.Component{
                         {/*TO-DO 联系我们的链接*/ }
                     </Col>
                     <Col span={4}>
-                        <Button type='link' ghost>联系我们</Button>
+                        <Contact></Contact>
                     </Col>
                 </Row>
         );
@@ -54,3 +55,42 @@ class Links extends React.Component{
 }
 
 export default Foot;
+
+
+
+const close = () => {
+  console.log(
+    'Notification was closed. Either the close button was clicked or duration time elapsed.',
+  );
+};
+const Contact = () => {
+  const [api, contextHolder] = notification.useNotification();
+  const openNotification = (placement) => {
+    const key = `open${Date.now()}`;
+    const btn = (
+      <Space>
+        <Button type="primary" size="small" onClick={() => api.destroy(key)}>
+          OK
+        </Button>
+      </Space>
+    );
+    
+    api.info({
+      message: '联系我们',
+      description:
+        '开发者邮箱: 1292437680@qq.com',
+      placement,
+      btn,
+      key,
+      onClose: close,
+    });
+  };
+  return (
+    <>
+      {contextHolder}
+      <Button className='contact' type="link" onClick={()=>openNotification('bottom')}>
+        联系我们
+      </Button>
+    </>
+  );
+};
